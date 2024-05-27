2b75885b19ad9e623ad9f07657d402d38ae22a4f
unknown <solomonpipi1@gmail.com>
2024-05-27 12:55:18 +0100

    created input component and added styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
-import Modal from "../components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal actionLabel="Submit" isOpen title="Sign Up" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
